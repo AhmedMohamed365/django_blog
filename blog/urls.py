6cded7319da8d871a5418c63717eaf9402775663
Ahmed Mohamed Hassan <56398966+AhmedMohamed365@users.noreply.github.com>
2024-11-27 00:19:25 +0200

    added Post  ,Author tables in first migration

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', view=views.Home,name='blog-home'),
+    path('about', view=views.about,name='blog-about'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -2,5 +2,26 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
+posts = [
+
+    {'author': 'Ahmed',
+     'title': 'how to use LLM in production',
+     'content': 'First post I created by hand',
+     'date_posted':'August 27 , 2018'
+     },
+
+     {'author': 'Hassan',
+     'title': 'how to play Uncharted 4',
+     'content': 'First post I created by hand',
+     'date_posted':'August 28 , 2018'
+     }
+]
+
 def Home(request):
-    return HttpResponse("<h1> Blog Home </h1>")
+    context = {
+        'posts': posts,
+        'title' : 'home'
+    }
+    return render(request,'blog/home.html',context=context)
+def about(request):
+    return render(request,'blog/about.html')
