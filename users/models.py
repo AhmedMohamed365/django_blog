13f4c6102f321a6b9b5a1085377ab7fc0b8cf5ea
Ahmed Mohamed Hassan <56398966+AhmedMohamed365@users.noreply.github.com>
2024-11-29 22:31:57 +0200

    feat: user picture now shows in main page along it's article

--- users/models.py
+++ users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image
 # Create your models here.
 class Profile(models.Model):
     user = models.OneToOneField(User,on_delete=models.CASCADE)
@@ -7,4 +8,10 @@ class Profile(models.Model):
     def __str__(self):
         return f'{self.user.username} Profile '
 
+    def save(self):
+        super().save()
+        img = Image.open(self.image.path)
+        if img.height > 150 or img.width > 150 :
+            img.thumbnail((150,150))
+            img.save(self.image.path)
 
