c05fc221e24fc42fa56294d0c3eb38c7b479d222
Ahmed Mohamed Hassan <56398966+AhmedMohamed365@users.noreply.github.com>
2024-11-29 14:05:47 +0200

    Added nav bar with (login-register-profile sections)

--- users/views.py
+++ users/views.py
@@ -2,11 +2,13 @@ from django.shortcuts import render , redirect
 from django.http import HttpRequest
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 def register(request:HttpRequest):
     if request.method =='POST':
        form = UserCreationForm(request.POST)
        if form.is_valid():
+           form.save()
            print('form is valid')
            username = form.cleaned_data.get('username')
            messages.success(request,f'account user {username} has registered!')
@@ -15,3 +17,7 @@ def register(request:HttpRequest):
         form = UserCreationForm()
 
     return render (request,'users/register.html',context={'form':form})
+
+@login_required
+def profile(request: HttpRequest):
+    return render(request,'users/profile.html')
