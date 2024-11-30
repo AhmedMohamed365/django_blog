540f2795b11e19c7cfc81de52bc1c1480e56720f
Ahmed Mohamed Hassan <56398966+AhmedMohamed365@users.noreply.github.com>
2024-11-30 07:05:54 +0200

    feat:Added CRUD operations for posts with classBasedViews

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,19 @@
 from django.urls import path
 
+from .views import(
+PostListView, 
+PostDetailView,
+PostCreateView,
+PostUpdateView,
+PostDeleteView
+ )
 from . import views
 urlpatterns = [
-    path('', view=views.Home,name='blog-home'),
-    path('about', view=views.about,name='blog-about'),
+    #app/model_list.html
+    path('', view=PostListView.as_view(),name='blog-home'),
+    path('post/new/', view=PostCreateView.as_view(),name='post-create'),
+    path('post/<int:pk>/update/', view=PostUpdateView.as_view(),name='post-update'),
+    path('post/<int:pk>/delete/', view=PostDeleteView.as_view(),name='post-delete'),
+    path('post/<int:pk>/', view=PostDetailView.as_view(),name='post-detail'),
+    path('about', view=views.about,name='blog-about'), 
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
 #imported models
+from django.views.generic import ListView , DetailView,CreateView,UpdateView,DeleteView
 from .models import Post
+from django.contrib.auth.mixins import LoginRequiredMixin , UserPassesTestMixin
 # Create your views here.
 
 
@@ -13,5 +14,57 @@ def Home(request):
         'title' : 'Home'
     }
     return render(request,'blog/home.html',context=context)
+
+# rewrite as class based view
+
+class PostCreateView(LoginRequiredMixin,CreateView):
+    model = Post
+    fields = ['title','content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class PostUpdateView(LoginRequiredMixin,UserPassesTestMixin,UpdateView):
+    model = Post
+    fields = ['title','content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        print(self.request.user)
+        return super().form_valid(form)
+    
+    # is it user who posted that post ?
+    def test_func(self):
+        post = self.get_object()
+        if post.author == self.request.user: 
+            return True
+        else:
+            return False
+        
+class PostDeleteView(LoginRequiredMixin,UserPassesTestMixin,DeleteView):
+    model = Post
+    # is it user who posted that post ?
+    def test_func(self):
+        post = self.get_object()
+        if post.author == self.request.user: 
+            return True
+        else:
+            return False
+    
+    
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+    
+class PostDetailView(DetailView):
+    model = Post
+    
+
+
+
 def about(request):
-    return render(request,'blog/about.html')
+    return render(request,'blog/about.html') 
+
