fbee56fd5d8905663255814e442eae9e82c71588
Ahmed Mohamed Hassan <56398966+AhmedMohamed365@users.noreply.github.com>
2024-12-09 19:09:30 +0200

    fixing static path in production

--- django_project/settings.py
+++ django_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!85mbsh05a73iep_whk3jnvh(pc(c9a5duivsr%rr#eh0s*+^('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -119,6 +119,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
 STATIC_URL = '/static/'
 
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap4"
