a4a7aa6c94b2b80f572fb578427275d135343982
Ahmed Mohamed Hassan <56398966+AhmedMohamed365@users.noreply.github.com>
2024-12-09 19:36:13 +0200

    deploy2

--- django_project/settings.py
+++ django_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-!85mbsh05a73iep_whk3jnvh(pc(c9a5duivsr%rr#eh0s*+^(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -119,8 +119,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR,'static')
-STATIC_URL = '/static/'
+# for deployment
+STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles/')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,'static/')
+]
+
 
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap4"
 
