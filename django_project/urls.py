03ea62197d101b76e1462e8ae4d957397c903918
Ahmed Mohamed Hassan <56398966+AhmedMohamed365@users.noreply.github.com>
2024-12-09 22:20:42 +0200

    adding whitenoise package to serve static files

--- django_project/urls.py
+++ django_project/urls.py
@@ -41,6 +41,10 @@ urlpatterns = [
 
 ] 
 
-if settings.DEBUG:
-    urlpatterns+=static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
+# Serve media files in production through WhiteNoise
+
+urlpatterns+=static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
+
+
+
 
